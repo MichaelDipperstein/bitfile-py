c4ce3ec30897c8e4c6ac0c6499726a8296f150fe
Michael Dipperstein <mdipper@alumni.engr.ucsb.edu>
2010-05-15 21:54:00 -0700

    Update setup to generate pretty PKG-INFO.

--- setup.py
+++ setup.py
@@ -1,5 +1,74 @@
 from distutils.core import setup
 
+__desc__  = """
+=============
+BitFile Class
+=============
+Description
+-----------
+This archive contains a python package that includes a class implementing
+bitwise reading and writing for sequential files.  This class is intended to be
+easy to follow and expand upon, though it may be used without an understanding
+of its implementation.
+
+License
+-------
+BitFile is licensed under the GNU General Public License v3.  See COPYING for
+full license text.
+
+Files
+-----
+::
+
+    __init__.py     - Python package initializtion code for bitfile.
+    bitfile.py      - Class implementing bitwise reading and writing for
+    sequential files.
+    COPYING         - GNU General Public License v3
+    README          - Package documentation
+    sample.py       - Sample usage and doctest.
+    setup.py        - distutils setup file.
+
+Installing
+----------
+This package uses distutils.  The package may be installed with the following
+command::
+
+    python setup.py install
+
+Usage
+-----
+bitfile.py is fully documented with docstrings.  Use your favorite tool for
+generating documentation from docstrings.
+
+sample.py demonstrates usage of each of the BitFile methods.
+
+sample.py also contains doctest information.  The doctest may be excuted with
+the following command::
+
+    python sample.py -v
+
+History
+-------
+| 01/14/10 - Initial release
+| 03/04/10 - Add support for 'r+' (read/write) mode
+
+ToDo
+----
+* Add constructor to convert a standard file into a BitFile.
+"""
+
+__classifiers__ = [
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: GNU General Public License (GPL)',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: System :: Archiving :: Compression',
+    'Topic :: Utilities',
+]
+
 setup(name='bitfile',
       version='0.2',
       description=\
@@ -11,8 +80,6 @@ setup(name='bitfile',
       packages=['bitfile',],
       package_data={'bitfile': ['COPYING', 'README']},
       platforms='All platforms',
-      long_description="""A simple class of I/O methods for files that contain
-        data in sizes that aren't integral bytes.  The methods contained in
-        this class were created with compression algorithms in mind, but may
-        be suited to other applications."""
+      long_description=__desc__,
+      classifiers=__classifiers__,
      )
